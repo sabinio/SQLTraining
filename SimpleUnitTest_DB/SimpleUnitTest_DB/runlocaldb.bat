e308aa0c4304d4b46bbe7fc69dca8d78f33bb42e
Richard Lee <Richard.Lee@sabin.io>
2016-01-29 15:28:33 +0000

    have added echo to get buildconfiguration arguement working on build

--- SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
+++ SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
@@ -2,5 +2,5 @@
 
 "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\Debug\SimpleUnitTest_DB.dacpac" /Profile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\Debug\localdb_SimpleUnitTest_DB.publish.publish.xml"
 
-ECHO "%OutDir%"
-ECHO "$(BuildConfiguration)"
+ECHO %1%
+ECHO "%1%"
