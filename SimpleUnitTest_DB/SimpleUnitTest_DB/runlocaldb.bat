330a5bff8ebc5fbb002651e4d755b56faf846d42
Richard Lee <Richard.Lee@sabin.io>
2016-01-29 15:14:29 +0000

    echo build configuration

--- SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
+++ SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
@@ -3,3 +3,4 @@
 "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\Debug\SimpleUnitTest_DB.dacpac" /Profile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\Debug\localdb_SimpleUnitTest_DB.publish.publish.xml"
 
 ECHO "%OutDir%"
+ECHO "$(BuildConfiguration)"
