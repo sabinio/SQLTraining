294408d0823e62d064c5ab941a83f48612ff1655
Richard Lee <Richard.Lee@sabin.io>
2016-01-29 13:52:45 +0000

    still trying to get variables correct for sqlpackage deployment

--- SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
+++ SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
@@ -1,3 +1,3 @@
 "%ProgramFiles%\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "ProjectsV12" 12.0 -s
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_BINARIESDIRECTORY%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\debug\SimpleUnitTest_DB.dacpac" /Profile:"%BUILD_BINARIESDIRECTORY%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\debug\localdb_SimpleUnitTest_DB.publish.publish.xml"
+"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_BINARIESDIRECTORY%\SimpleUnitTest_DB\SimpleUnitTest_DB.dacpac" /Profile:"%BUILD_BINARIESDIRECTORY%\SimpleUnitTest_DB\localdb_SimpleUnitTest_DB.publish.publish.xml"
