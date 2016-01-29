f6f7d05e55f8df7975a628007f8a2418b4785f6e
Richard Lee <Richard.Lee@sabin.io>
2016-01-29 15:52:50 +0000

    Updated runlocaldb.bat

--- SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
+++ SimpleUnitTest_DB/SimpleUnitTest_DB/runlocaldb.bat
@@ -1,4 +1,1 @@
 "%ProgramFiles%\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "ProjectsV12" 12.0 -s
-
-"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\%1\SimpleUnitTest_DB.dacpac" /Profile:"%BUILD_REPOSITORY_LOCALPATH%\SimpleUnitTest_DB\SimpleUnitTest_DB\bin\%1\localdb_SimpleUnitTest_DB.publish.publish.xml"
-
