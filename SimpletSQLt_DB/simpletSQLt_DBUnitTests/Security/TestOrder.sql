ba41bfca71cfba7503ada098793bc069007b4416
Richard Lee <Richard.Lee@sabin.io>
2016-01-29 16:37:30 +0000

    adding missing schema

---
+++ SimpletSQLt_DB/simpletSQLt_DBUnitTests/Security/TestOrder.sql
@@ -0,0 +1,9 @@
+﻿CREATE SCHEMA [TestOrder]
+    AUTHORIZATION [dbo];
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'TestOrder';
+
