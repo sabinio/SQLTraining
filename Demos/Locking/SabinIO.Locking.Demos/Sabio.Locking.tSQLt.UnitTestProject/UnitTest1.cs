82259d19436c9fa13cb3e24671f5a88912b45060
Richard Lee <Richard.Lee@sabin.io>
2016-02-02 11:10:35 +0000

    altered unit test project so that tSQLt database is a dacpac in the unit test project. Also added unit test wrapper project so that test results are part of the build

---
+++ Demos/Locking/SabinIO.Locking.Demos/Sabio.Locking.tSQLt.UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tSQLt.Client.Net;
+
+namespace tSQLtTestProjects
+{
+    [TestClass]
+    public class MSTest_ExampleTests
+    {
+
+        private readonly tSQLtTestRunner _runner =
+            new tSQLtTestRunner("server=(localdb)\\ProjectsV12;Integrated Security=True;initial catalog=SabinIO.Locking.tSQLt.UnitTests;", 60 * 1000/*optional timeout, default is 2 minutes*/);
+        //new tSQLtTestRunner("server=giratina;Integrated Security=True;initial catalog=SimpleUnitTest_DB;", 60 * 1000/*optional timeout, default is 2 minutes*/); 
+
+        [TestMethod]
+        public void TestHeapLockHierarchy()
+        {
+            var result = _runner.Run("TestLockingHierarchy", "TestHeapLockHierarchy");
+            Assert.IsTrue(result.Passed());
+        }
+
+       
+    }
+}
