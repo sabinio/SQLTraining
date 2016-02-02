83550870bf40ee1111b88db0af0984a47fdbb732
Richard Lee <Richard.Lee@sabin.io>
2016-02-02 12:27:39 +0000

    altered vs edition to a variable on the build, as need to pass in as an argument to the bat file, which has been updated to accept new argument.

--- SimpletSQLt_DB/SimpletSQLt_DB/SimpletSQLt_DB_sqlpackage.bat
+++ SimpletSQLt_DB/SimpletSQLt_DB/SimpletSQLt_DB_sqlpackage.bat
@@ -1,4 +1,1 @@
-"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_REPOSITORY_LOCALPATH%\SimpletSQLt_DB\SimpletSQLt_DB\bin\%2\SimpletSQLt_DB.dacpac" /Profile:"%BUILD_REPOSITORY_LOCALPATH%\SimpletSQLt_DB\SimpletSQLt_DB\bin\%2\localdb_SimpletSQLt_DB.publish.publish.xml"
-ECHO ON
-ECHO %1
-ECHO "%1"
+"%ProgramFiles(x86)%\Microsoft Visual Studio %1.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe" /Action:Publish /SourceFile:"%BUILD_REPOSITORY_LOCALPATH%\SimpletSQLt_DB\SimpletSQLt_DB\bin\%2\SimpletSQLt_DB.dacpac" /Profile:"%BUILD_REPOSITORY_LOCALPATH%\SimpletSQLt_DB\SimpletSQLt_DB\bin\%2\localdb_SimpletSQLt_DB.publish.publish.xml"
