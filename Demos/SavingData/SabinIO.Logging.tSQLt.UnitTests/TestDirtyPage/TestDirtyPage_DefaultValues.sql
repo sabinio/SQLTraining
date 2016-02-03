db18f32c0d015b06eb66c2ae37c5eee737e941a9
Richard Lee <Richard.Lee@sabin.io>
2016-02-03 18:21:15 +0000

    added file that seemedto not have been included in prior check in

---
+++ Demos/SavingData/SabinIO.Logging.tSQLt.UnitTests/TestDirtyPage/TestDirtyPage_DefaultValues.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE TestDirtyPage.TestDirtyPage_DefaultValues
+AS
+
+BEGIN
+
+declare @Actual NVARCHAR (10)
+insert into WriteLogTest
+Select 'CleanPage'
+
+--Ensure written to disk
+Checkpoint
+
+select @Actual = StringColumn FROM WriteLogTest
+
+--Check value 
+EXEC tSQLt.AssertEquals 'CleanPage'
+		,@Actual
+
+END
