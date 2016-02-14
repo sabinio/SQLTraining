c6e3e710d287e1ef19031b422ab6334dfdf57d8b
Richard Lee <Richard.Lee@sabin.io>
2016-02-14 21:18:41 +0000

    updated forced params demo to execute in both forced and simple as required.

--- Demos/QueryExecution/QueryExecution/dbo/Tables/FP_Sales.sql
+++ Demos/QueryExecution/QueryExecution/dbo/Tables/FP_Sales.sql
@@ -24,6 +24,11 @@
     [TotalDue]               MONEY            NOT NULL,
     [Comment]                NVARCHAR (128)   COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
     [rowguid]                UNIQUEIDENTIFIER NOT NULL,
-    [ModifiedDate]           DATETIME         NOT NULL
+    [ModifiedDate]           DATETIME         NOT NULL,
+	CONSTRAINT [PK_FP_Sales_FP_Sales_ID] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC
+)
 );
+
 
