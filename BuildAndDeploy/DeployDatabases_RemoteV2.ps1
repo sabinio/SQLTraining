efb9c2d88044cd4c0620fa2b637085cf174205e2
Richard Lee <Richard.Lee@sabin.io>
2016-02-15 14:33:03 +0000

    Updated DeployDatabases_RemoteV2.ps1

--- BuildAndDeploy/DeployDatabases_RemoteV2.ps1
+++ BuildAndDeploy/DeployDatabases_RemoteV2.ps1
@@ -9,7 +9,7 @@ Foreach-Object{
 	$DACPAC = $_.FullName
 	$ProfilePath = join-Path ($_.Directory) ('..\..\')
 
-	Get-ChildItem $ProfilePath -Filter *.publish.xml -Exclude *tSQLt*,*test*,*Test* | `
+	Get-ChildItem $ProfilePath -Filter *.publish.xml | `
 	Foreach-Object{
 		# This will take the last publish profile if there are mutliple
 		$Profile = $_.FullName
