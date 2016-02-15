6346d8d8a06e741ea7b79c8d5fc4b3a1d4cc87d9
Richard Lee <Richard.Lee@sabin.io>
2016-02-15 13:50:28 +0000

    Updated DeployDatabases_RemoteV2.ps1

--- BuildAndDeploy/DeployDatabases_RemoteV2.ps1
+++ BuildAndDeploy/DeployDatabases_RemoteV2.ps1
@@ -4,7 +4,7 @@ param([string]$ServerName, [string]$DropFolder, [string]$SQLPassword)
 
 	
 
-Get-ChildItem $DropFolder -Filter *.dacpac -Exclude *tSQLt*,*test*,*Test* -Recurse | `
+Get-ChildItem $DropFolder -Exclude *tSQLt*,*test*,*Test* -Filter *.dacpac -Recurse | `
 Foreach-Object{
 	$DACPAC = $_.FullName
 	$ProfilePath = join-Path ($_.Directory) ('..\..\')
