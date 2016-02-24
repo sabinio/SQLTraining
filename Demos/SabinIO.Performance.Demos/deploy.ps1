a5d9411ad7cb80420e7108c7ce7167474b58f832
Richard Lee <Richard.Lee@sabin.io>
2016-02-24 11:38:41 +0000

    created batfiles and powershell file; aim is to deploy via PoweShell; updated publish profiles to recreate database each time

---
+++ Demos/SabinIO.Performance.Demos/deploy.ps1
@@ -0,0 +1,24 @@
+param([string]$ServerName, [string]$DropFolder, [string]$SQLPassword)
+
+[string] $sqlpackage = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"
+
+	
+
+Get-ChildItem $DropFolder -Exclude *tSQLt*,*test*,*Test*,*master* -Filter *.dacpac -Recurse | `
+Foreach-Object{
+	$DACPAC = $_.FullName
+	$ProfilePath = join-Path ($_.Directory) ('..\..\')
+
+	Get-ChildItem $ProfilePath -Filter *local.publish.xml | `
+	Foreach-Object{
+		# This will take the last publish profile if there are mutliple
+		$Profile = $_.FullName
+	}
+
+	$vars = ('/a:publish'), ('/sf:' + $DACPAC), ('/pr:' + $PROFILE), ('/TargetServerName:' + $ServerName), ('/TargetUser:SQLTraining'), ('/TargetPassword:'+$SQLPassword)
+
+	write-host "Deploying model for database " $DACPAC  -foregroundcolor green -backgroundcolor black 
+		& $SQLPackage $vars
+	if (! $?) { throw "Deploy failed" }
+}
+
