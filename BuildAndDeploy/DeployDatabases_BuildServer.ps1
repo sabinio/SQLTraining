a7cdaa08b5d776448d3acee278aebd4ecb0501e1
Richard Lee <Richard.Lee@sabin.io>
2016-02-24 14:05:19 +0000

    moved powershell script out of performance solution and moved to build and deploy location so that it is not dependent on a particualr solution

---
+++ BuildAndDeploy/DeployDatabases_BuildServer.ps1
@@ -0,0 +1,24 @@
+param([string]$ServerName, [string]$DropFolder)
+
+[string] $sqlpackage = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"
+
+	
+
+Get-ChildItem $DropFolder -Exclude *tSQLt*,*test*,*Test*,*master*,*db* -Filter *.dacpac -Recurse | `
+Foreach-Object{
+	$DACPAC = $_.FullName
+	$ProfilePath = join-Path ($_.Directory) ('..\..\')
+
+	Get-ChildItem $ProfilePath -Filter *local.publish.xml | `
+	Foreach-Object{
+		# This will take the last publish profile if there are mutliple
+		$Profile = $_.FullName
+	}
+
+	$vars = ('/a:publish'), ('/sf:' + $DACPAC), ('/pr:' + $PROFILE), ('/TargetServerName:' + $ServerName)
+
+	write-host "Deploying model for database "$DACPAC
+		& $SQLPackage $vars
+	if (! $?) { throw "Deploy failed" }
+}
+
