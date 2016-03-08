32107ce49ef2a7cab2cae976f5a74f3e55a42675
Richard Lee <Richard.Lee@sabin.io>
2016-03-08 17:20:23 +0000

    increase timeout for test command and reduced number of executes so that the test has a chnce of running on the build server

--- Demos/SabinIO.Performance.Demos/SabinIO.Performance.UnitTesting/TruncateDelete.cs
+++ Demos/SabinIO.Performance.Demos/SabinIO.Performance.UnitTesting/TruncateDelete.cs
@@ -28,8 +28,12 @@ namespace SabinIO.Performance.UnitTesting
 
             for (int i = 0; i < batches.Length; i++)
             {
+
+                batches[1] = batches[1].Replace("1000", "100");
+                batches[3] = batches[3].Replace("1000", "100");
                 _conn.Open();
                 SqlCommand cmd_1 = new SqlCommand(batches[i], _conn);
+                cmd_1.CommandTimeout = 60;
                 cmd_1.ExecuteNonQuery();
                 _conn.Close();
             }
