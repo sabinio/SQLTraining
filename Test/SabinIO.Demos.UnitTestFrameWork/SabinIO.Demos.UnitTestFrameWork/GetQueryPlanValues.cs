1eea8fba697c07df1bb890cd464c35a3135d8cb2
Richie Lee <richie@phoebix.com>
2016-03-21 11:47:21 +0000

    move testing framework out of demo folder so that builds do not cause a ci build of the demos; will create a separate build for this solution with aim to create and publish a nuget package to the packages tab

---
+++ Test/SabinIO.Demos.UnitTestFrameWork/SabinIO.Demos.UnitTestFrameWork/GetQueryPlanValues.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+
+namespace SabinIO.Demos.UnitTestFrameWork
+{
+    public class GetQueryPlanValues
+    {
+
+        public XPathNodeIterator GetXpathIterator(string QueryPlan, string SqlNamespace, string xpath)
+        {
+            //read query plan to tring, then xml reader
+            StringReader strReader = new StringReader(QueryPlan);
+            XmlTextReader xreader = new XmlTextReader(strReader);
+
+            //xpath document from the xreader, preserving whitespace, 
+            //then a nvaigator so that we can search using xpath
+            XPathDocument doc = new XPathDocument(xreader, XmlSpace.Preserve);
+            XPathNavigator navigator = doc.CreateNavigator();
+
+            //define namespace
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(navigator.NameTable);
+            nsmgr.AddNamespace("sql", SqlNamespace);
+
+            //create xpath expression to search
+            XPathExpression xpression;
+            xpression = navigator.Compile(xpath);
+            //set namespace in the expression
+            xpression.SetContext(nsmgr);
+            //iterate over the nodes
+            XPathNodeIterator iterator =  navigator.Select(xpression);
+            return iterator;
+        }
+
+
+        public string GetSumOfValues(string QueryPlan, string SqlNamespace, string xpath)
+        {
+            try
+            {
+
+                string _QueryPlan = QueryPlan;
+                string _SqlNameSpace = SqlNamespace;
+                string _xpath = xpath;
+
+                XPathNodeIterator i = GetXpathIterator(_QueryPlan, _SqlNameSpace, _xpath);
+
+                //get total value of each of the expressions we are looking for in the node
+                Single TotalValue = 0;
+                while (i.MoveNext()) TotalValue += Single.Parse(i.Current.Value);
+                string ret = String.Empty;
+                ret = Convert.ToString(TotalValue);
+                return ret;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                return "-1";
+            }
+        }
+
+             public string GetSingleValue(string QueryPlan, string SqlNamespace, string xpath)
+        {
+            try
+            {
+                string _QueryPlan = QueryPlan;
+                string _SqlNameSpace = SqlNamespace;
+                string _xpath = xpath;
+
+                XPathNodeIterator i = GetXpathIterator(_QueryPlan, _SqlNameSpace, _xpath);
+
+                string ret = String.Empty;
+                while (i.MoveNext()) ret = i.Current.Value;
+                return ret;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                return "-1";
+            }
+        }
+    }
+}
---
+++ Test/SabinIO.Demos.UnitTestFrameWork/SabinIO.Demos.UnitTestFrameWork/GetSqlInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SabinIO.Demos.UnitTestFrameWork
+{
+    public class GetSqlInfo
+    {
+
+        public List<string> SqlInfoMessages = new List<string>();
+
+        public void Message(object sender, SqlInfoMessageEventArgs e)
+        {
+            int i;
+            for (i = 0; i < e.Errors.Count; i++)
+            {
+                SqlInfoMessages.Add(e.Errors[i].Message);
+            }
+        }
+    }
+}
