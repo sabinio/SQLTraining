7b8f5d0e56e429d9b7cd491844264689d061d0c5
Simon Sabin <simon@sabin.io>
2020-01-21 11:49:15 +0000

    remove migration demos

--- Migrations/Demo1/Scripts/DeployLocal.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-[CmdletBinding()]param(
-    $database = "WideWorldImporters"
-)
-$ErrorActionPreference="Stop"
-
-
-& $psscriptroot\Deploy.ps1 -Database $database -instance "DEFAULT" -server "localhost"
